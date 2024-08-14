d52eb6afe52288c526a99c7310dfd65cd3ec5438
payetools <143715134+payetools@users.noreply.github.com>
2024-08-14 13:26:54 +0200

    feat: added signalr extension method

---
+++ src/Codefactors.DataFabric.Transport.SignalR/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2024, Codefactors Ltd.
+//
+// Codefactors Ltd licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+using Codefactors.DataFabric.Subscriptions;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Codefactors.DataFabric.Transport.SignalR.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class IServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the SignalR transport as the data fabric notification transport.
+    /// </summary>
+    /// <param name="services">This <see cref="IServiceCollection"/>.</param>
+    /// <param name="initialiseSignalR">Optional parameter; set to false if AddSignalR() is being called elsewhere.</param>
+    /// <returns><see cref="IServiceCollection"/> supplied at invocation.</returns>
+    public static IServiceCollection AddSignalRTransport(this IServiceCollection services, bool initialiseSignalR = true)
+    {
+        // Required to ensure SignalR uses the subscription key rather than the regular user id
+        services.AddSingleton<IUserIdProvider, SignalRUserIdProvider>();
+
+        if (initialiseSignalR)
+            services.AddSignalR();
+
+        services.AddSingleton<IDataFabricTransport, SignalRTransport>();
+        services.AddSingleton<ISubscriptionFactory, SignalRSubscriptionFactory>();
+
+        return services;
+    }
+}
---
+++ src/Codefactors.DataFabric/Subscriptions/IClaimsBasedSubscriptionKeyGenerator.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2024, Codefactors Ltd.
+//
+// Codefactors Ltd licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+using System.Security.Claims;
+
+namespace Codefactors.DataFabric.Subscriptions;
+
+/// <summary>
+/// Interface for generating subscription keys based on claims rather than request context.
+/// </summary>
+public interface IClaimsBasedSubscriptionKeyGenerator
+{
+    /// <summary>
+    /// Generates a subscription key from the supplied claims.
+    /// </summary>
+    /// <param name="claims">Claims to extract key information from.</param>
+    /// <returns>Subscription key based on claims.</returns>
+    string GenerateKey(IEnumerable<Claim> claims);
+}
