a8f917c1d96d08536b7329bcea179e6465620b0c
payetools <143715134+payetools@users.noreply.github.com>
2024-08-14 15:20:57 +0200

    misc: renamed the solution

---
+++ src/Codefactors.DataFabric.Transport.SignalR/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2024, Codefactors Ltd.
+//
+// Codefactors Ltd licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Codefactors.DataFabric.Transport.SignalR.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="WebApplication"/>.
+/// </summary>
+public static class WebApplicationExtensions
+{
+    /// <summary>
+    /// Installs the SignalR middleware that allows WebSocket connections to correctly handle
+    /// authentication (as WebSockets don't support HTTP headers).
+    /// </summary>
+    /// <param name="webApplication">This <see cref="WebApplication"/> instance.</param>
+    /// <returns>Original <see cref="WebApplication"/> instance.</returns>
+    public static WebApplication UseSignalRAuthenticationMiddleware(this WebApplication webApplication)
+    {
+        return webApplication;
+    }
+}
